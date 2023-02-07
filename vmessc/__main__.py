b58bf994bbc821e66c899e4e3eaf4a3c892d4a3e
vhqr0 <zq_cmd@163.com>
2023-02-07 15:34:29 +0800

    main argparse update

--- vmessc/__main__.py
+++ vmessc/__main__.py
@@ -4,15 +4,23 @@ from .cli import VmessCli
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog='vmessc',
+        description='vmess proxy protocol client',
+    )
     parser.add_argument('-c', '--config-file', default='config.json')
+    parser.add_argument('command', nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
     config_file = args.config_file
+    command = args.command
 
     cli = VmessCli(config_file=config_file)
     try:
-        cli.cmdloop()
+        if command:
+            cli.onecmd(' '.join(command))
+        else:
+            cli.cmdloop()
     except KeyboardInterrupt:
         print('keyboard quit')
 
