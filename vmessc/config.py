4aaddcfffa6246ea3cfec289a94ec67cad568f56
vhqr0 <zq_cmd@163.com>
2023-02-08 15:33:58 +0800

    add subscribe format wiki url

--- vmessc/config.py
+++ vmessc/config.py
@@ -3,6 +3,9 @@
 Easy to fetch and manage subscribed-ed vmess nodes, and start client
 from a config file.
 
+Subscribe format:
+https://github.com/2dust/v2rayN/wiki/%E5%88%86%E4%BA%AB%E9%93%BE%E6%8E%A5%E6%A0%BC%E5%BC%8F%E8%AF%B4%E6%98%8E(ver-2)
+
 Config example:
 
   {
